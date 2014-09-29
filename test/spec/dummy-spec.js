03557e025656914c7757e0cb0486e46a45140cc0
auz34 <andrewzavgorodniy@gmail.com>
2014-09-29 16:54:09 -0700

    achieve 100% coverage

--- test/spec/dummy-spec.js
+++ test/spec/dummy-spec.js
@@ -1,7 +1,7 @@
 var dummy = require('../../src/dummy');
 
 describe('calculation', function() {
-    it('should sum arguments if both are uneven', function() {
+    it('should sum arguments if both are even', function() {
         var result = dummy.calculate(6, 10);
         expect(result).toBe(16);
     });
@@ -14,5 +14,15 @@ describe('calculation', function() {
     it('should divide b on a when only first argument one is uneven', function() {
         var result = dummy.calculate(3, 6);
         expect(result).toBe(2);
+    });
+
+    it('should multiply arguments if both are uneven', function() {
+        var result = dummy.calculate(7, 3);
+        expect(result).toBe(21);
+    });
+
+    it('should return zero if one of the arguments is not an integer', function() {
+        var result = dummy.calculate(7, '3');
+        expect(result).toBe(0);
     });
 });
