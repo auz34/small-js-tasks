1a99c7cb121ed9b6dc335ac5728a7499f73a8ce8
auz34 <andrewzavgorodniy@gmail.com>
2014-09-29 16:39:22 -0700

    use istanbul to estimate code coverage

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-jasmine-node-coverage');
+
+    grunt.initConfig({
+        jasmine_node: {
+            coverage: {
+
+            },
+            options: {
+                forceExit: true,
+                match: '.',
+                matchall: false,
+                extensions: 'js',
+                specNameMatcher: 'spec',
+                captureExceptions: true,
+                junitreport: {
+                    report: false,
+                    savePath : "./build/reports/jasmine/",
+                    useDotNotation: true,
+                    consolidate: true
+                }
+            }
+        }
+    });
+};
