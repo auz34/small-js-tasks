450ee0197fe1bc49c6785a1190b24aba20547ca3
auz34 <andrewzavgorodniy@gmail.com>
2014-09-29 15:55:05 -0700

    addition to previous commit

--- test/spec/dummy-spec.js
+++ test/spec/dummy-spec.js
@@ -1,3 +1,18 @@
-/**
- * Created by andrii.zavgorodnii on 9/29/2014.
- */
+var dummy = require('../../src/dummy');
+
+describe('calculation', function() {
+    it('should sum arguments if both are uneven', function() {
+        var result = dummy.calculate(6, 10);
+        expect(result).toBe(16);
+    });
+
+    it('should subtract arguments if only second one is uneven', function() {
+        var result = dummy.calculate(20, 7);
+        expect(result).toBe(13);
+    });
+
+    it('should divide b on a when only first argument one is uneven', function() {
+        var result = dummy.calculate(3, 6);
+        expect(result).toBe(2);
+    });
+});
