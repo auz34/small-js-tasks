6c140856830a643b18a1b1df294d1ff420db6ee5
auz34 <andrewzavgorodniy@gmail.com>
2014-10-02 10:29:03 -0700

    quick find tested and implemented

--- src/union-find.js
+++ src/union-find.js
@@ -18,4 +18,35 @@ UnionFind.QuickFind = function(n) {
     this.itemsMap = u.range(n);
 };
 
+u.extend(UnionFind.QuickFind.prototype, {
+    /**
+     * Returns true if two items are connected.
+     * @param p number of the first item.
+     * @param q number of the first item.
+     * @returns {boolean}
+     */
+    connected: function(p, q) {
+        return this.itemsMap[p] === this.itemsMap[q];
+    },
+
+    /**
+     * Unions two items into one group
+     * @param p
+     * @param q
+     */
+    union: function(p, q) {
+        if (this.connected(p, q)) {
+            return;
+        }
+
+        var numToReplace = this.itemsMap[p];
+        for (var i=0; i<this.itemsMap.length; i++) {
+            if (this.itemsMap[i] === numToReplace) {
+                this.itemsMap[i] = this.itemsMap[q];
+            }
+        }
+    }
+});
+
+
 module.exports = UnionFind;
--- test/spec/union-find-spec.js
+++ test/spec/union-find-spec.js
@@ -7,4 +7,19 @@ describe('quick-find', function() {
         var solver = new ufNS.QuickFind(10);
         expect(solver.itemsMap).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     });
+
+    it('should union items', function() {
+        var solver = new ufNS.QuickFind(10);
+        solver.union(5, 6);
+        var result = solver.connected(5, 6);
+        expect(result).toBe(true);
+    });
+
+    it('should union items transitively', function() {
+        var solver = new ufNS.QuickFind(10);
+        solver.union(5, 6);
+        solver.union(6, 7);
+        var result = solver.connected(5, 7);
+        expect(result).toBe(true);
+    });
 });
